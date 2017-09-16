bf8cfd51e2bf4c0a0890a98d515eaa0bf492dd2a
Jon Gacnik <jon@folderstudio.com>
2017-09-16 12:30:42 -0700

    format

--- index.js
+++ index.js
@@ -7,20 +7,15 @@ module.exports = box
 
 function box (name, key) {
   if (key && cache[key]) {
-    // console.log('key provided, fetching cached component', key)
     return cache[key]
   } else if (key) {
-    // console.log('key provided, creating new component', key)
     cache[key] = components[name]()
     return cache[key]
-  } else if (name && cache[name]) {
-    // console.log('name provided, fetching cached component', name)
-    return cache[name]
   } else if (key === false) {
-    // console.log('force uncached component', name)
     return components[name]() 
+  } else if (name && cache[name]) {
+    return cache[name]
   } else {
-    // console.log('name provided, creating new component', name)
     cache[name] = components[name]()
     return cache[name]
   }
