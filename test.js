7a1be5ee1693eef5e216b25adf6e2c98fa4a1e1d
Jon Gacnik <jon@folderstudio.com>
2017-09-16 13:39:46 -0700

    tests

---
+++ test.js
@@ -0,0 +1,49 @@
+var test = require('tape')
+var c = require('.')
+
+test('c.use()', function (t) {
+  c.use({
+    component: () => prop => console.log(prop)
+  })
+
+  var box = c._inspect()
+
+  t.ok('component' in box.components, '`component` passed into box')
+  t.end()
+})
+
+test('c(\'component\')', function (t) {
+  c('component')
+
+  var box = c._inspect()
+
+  t.ok('component' in box.cache, '`component` created and cached as `component`')
+  t.end()
+})
+
+test('c(\'component\', \'custom\')', function (t) {
+  c('component', 'custom')
+
+  var box = c._inspect()
+
+  t.ok('custom' in box.cache, '`component` created and cached as `custom`')
+  t.end()
+})
+
+test('c.delete(\'component\')', function (t) {
+  c.delete('component')
+
+  var box = c._inspect()
+
+  t.notOk('component' in box.cache, '`component` uncached')
+  t.end()
+})
+
+test('c(\'component\', false)', function (t) {
+  c('component', false)
+
+  var box = c._inspect()
+
+  t.notOk('component' in box.cache, '`component` created and not cached')
+  t.end()
+})
